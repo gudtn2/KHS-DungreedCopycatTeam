201439a9c28f2170a955e2527cb772b2c7addd9d
gudtn2 <tyrueu@gmail.com>
2023-11-08 01:01:43 +0900

    aaaa

---
+++ Unity2D_DungreedCopy/Assets/Scripts/Weapon/SwingEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingEvent : MonoBehaviour
+{
+    public void DestroySwing()
+    {
+        Destroy(this.gameObject);
+    }
+}
