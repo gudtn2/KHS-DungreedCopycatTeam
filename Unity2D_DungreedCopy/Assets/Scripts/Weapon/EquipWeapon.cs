a8b7176a5a5b68be9082d138ad45e33ac6acd402
gudtn2 <tyrueu@gmail.com>
2023-11-07 23:50:24 +0900

    1107-Weapon
    Swing조건 추가, 무기 위치 1,2 지정 및 기능 추가

--- Unity2D_DungreedCopy/Assets/Scripts/Weapon/EquipWeapon.cs
+++ Unity2D_DungreedCopy/Assets/Scripts/Weapon/EquipWeapon.cs
@@ -7,38 +7,52 @@ public class EquipWeapon : MonoBehaviour
     [Header("무기 종류")]
     private KeyCode weapon1 = KeyCode.Alpha1;
     public GameObject[] Weapons;
-    public bool[] hasWeapons;
+    public bool hasWeapons1;
+    public bool hasWeapons2;
+    public int equipWeapon = 0;
+    
     public Weapon weapon;
     public Sprite[] sprites;
 
     public GameObject MeleePos1;
-    public SpriteRenderer MeleeSpr1;
     public GameObject MeleePos2;
-    public SpriteRenderer MeleeSpr2;
     public bool SwitchPos = true;
 
     void Awake()
     {
-        //weapon.GetComponent<Weapon>();
-        MeleeSpr1.GetComponent<SpriteRenderer>();
-        MeleeSpr2.GetComponent<SpriteRenderer>();
+        weapon = Weapons[0].GetComponent<Weapon>();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Weapons[0] != null)
+        {
+            hasWeapons1 = true;
+        }
+        else if(Weapons[0] == null)
+        {
+            hasWeapons1 = false;
+        }
+
+        if(Input.GetKeyDown(weapon1) && hasWeapons1 == true && weapon.type == Weapon.Type.Melee)
+        {
+            MeleePos1.SetActive(true);
+            equipWeapon = 1;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && equipWeapon != 0)
         {
             if (SwitchPos == true)
             {
                 //MeleeSpr1.sprite = sprites[weapon.value];
-                MeleePos1.SetActive(true);
-                MeleePos2.SetActive(false);
+                MeleePos1.SetActive(false);
+                MeleePos2.SetActive(true);
                 SwitchPos = false;
             }
             else if (SwitchPos == false)
             {
-                MeleePos1.SetActive(false);
-                MeleePos2.SetActive(true);
+                MeleePos1.SetActive(true);
+                MeleePos2.SetActive(false);
                 SwitchPos = true;
             }
         }
