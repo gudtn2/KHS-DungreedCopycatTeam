8464baad661e6d5d6a8c3e519902bc252e7dbe75
gudtn2 <tyrueu@gmail.com>
2023-11-12 05:26:58 +0900

    1111-Inventory
    인벤토리 기능 구현

---
+++ Unity2D_DungreedCopy/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public List<Item> items = new List<Item>();
+
+    public delegate void OnChangeItem();
+    public OnChangeItem onChangeItem;
+
+    private int slotCnt;
+    public int SlotCnt
+    {
+        get => slotCnt;
+        set
+        {
+            slotCnt = value;
+        }
+    }
+    private void Start()
+    {
+        SlotCnt = 15;
+    }
+    public bool AddItem(Item _item)
+    {
+        if(items.Count < SlotCnt)
+        {
+            items.Add(_item);
+            if(onChangeItem != null)
+            onChangeItem.Invoke();
+            return true;
+        }
+        return false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("FieldItem"))
+        {
+            FieldItems fieldItems = collision.GetComponent<FieldItems>();
+            if (AddItem(fieldItems.GetItem()))
+                fieldItems.DestroyItem();
+        }
+    }
+}
---
+++ Unity2D_DungreedCopy/Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    Inventory inven;
+
+    public GameObject inventoryPanel;
+
+    public Slot[] slots;
+    public Transform slotHolder;
+    bool activeInventory = false;
+
+    private void Start()
+    {
+        slots = slotHolder.GetComponentsInChildren<Slot>();
+        inven = Inventory.instance;
+        inven.onChangeItem += RedrawSlotUI;
+        inventoryPanel.SetActive(activeInventory);
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+
+    void RedrawSlotUI()
+    {
+        for(int i = 0; i < slots.Length; i++)
+        {
+            slots[i].RemoveSlot();
+        }
+        for (int i = 0; i < inven.items.Count; i++)
+        {
+            slots[i].item = inven.items[i];
+            slots[i].UpdateSlotUI();
+        }
+
+    }
+}
