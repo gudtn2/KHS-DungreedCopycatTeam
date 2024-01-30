743eada64e3c491475f70b4e83020489e5da8751
gudtn2 <tyrueu@gmail.com>
2024-01-31 01:40:15 +0900

    summon

--- Unity2D_DungreedCopy/Assets/Scripts/Inventory2/Model/ItemParameters/ItemParameterSO.cs
+++ Unity2D_DungreedCopy/Assets/Scripts/Inventory2/Model/ItemParameters/ItemParameterSO.cs
@@ -7,4 +7,9 @@ public class ItemParameterSO : ScriptableObject
 {
     [field: SerializeField]
     public string ParameterName { get; private set; }
+
+    [field: SerializeField]
+    public int Damage { get; set; }
+    [field: SerializeField]
+    public float AttackSpeed { get; set; }
 }
