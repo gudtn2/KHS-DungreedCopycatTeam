588dd34e436790b10c6419fc85e0d2e772d9b7f6
Seoptank <cnpdi1118@naver.com>
2024-03-22 02:13:33 +0900

    aaaa

---
+++ Unity2D_DungreedCopy/Assets/Scripts/Weapon/EquipWeapon.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipWeapon : MonoBehaviour
+{
+    [Header("무기 종류")]
+    private KeyCode weapon1 = KeyCode.Alpha1;
+    public GameObject[] Weapons;
+    public bool hasWeapons1;
+    public bool hasWeapons2;
+    public int equipWeapon = 0;
+    
+    public Weapon weapon;
+    public Sprite[] sprites;
+
+    public GameObject MeleePos1;
+    public GameObject MeleePos2;
+    public bool SwitchPos = true;
+    void Awake()
+    {
+        weapon = Weapons[0].GetComponent<Weapon>();
+    }
+
+    void Update()
+    {
+        if(Weapons[0] != null)
+        {
+            hasWeapons1 = true;
+        }
+        else if(Weapons[0] == null)
+        {
+            hasWeapons1 = false;
+        }
+        if(!PlayerController.instance.onUI)
+        {
+
+            if(Input.GetKeyDown(weapon1) && hasWeapons1 == true && weapon.type == Weapon.Type.Melee)
+            {
+                MeleePos1.SetActive(true);
+                equipWeapon = 1;
+
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0) && equipWeapon != 0 && PlayerController.instance.canAttack)
+            {
+                if (SwitchPos == true)
+                {
+                    //MeleeSpr1.sprite = sprites[weapon.value];
+                    MeleePos1.SetActive(false);
+                    MeleePos2.SetActive(true);
+                    SwitchPos = false;
+                }
+                else if (SwitchPos == false)
+                {
+                    MeleePos1.SetActive(true);
+                    MeleePos2.SetActive(false);
+                    SwitchPos = true;
+                }
+            }
+        }
+    }
+}
