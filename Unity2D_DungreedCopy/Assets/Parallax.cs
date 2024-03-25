18a7cc796134ea06e0ca1a2646f8537e262aa50b
Seoptank <cnpdi1118@naver.com>
2024-03-25 10:58:23 +0900

    aaaa

--- Unity2D_DungreedCopy/Assets/Parallax.cs
+++ Unity2D_DungreedCopy/Assets/Parallax.cs
@@ -4,67 +4,34 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private Transform   transformCam;
-    private Vector3     camStartPos;
-    private float       dis;
-
-    private Material[]          mats;
-    private GameObject[]        backgrounds;
-    private float[]             backSpeed;
-
-    private float farthestBack;
-    [Range(0.01f,0.05f)]
     [SerializeField]
-    private float parallaxSpeed;
+    private Transform camTransform;
 
+    private Vector3 camStartPos;
+    private float   dis;
+    
+    private Material[]  materials;
+    private float[]     layerSpeed;
+
+    [SerializeField]
+    [Range(0.01f, 1.0f)]
+    private float parallaxSpeed;
 
     private void Awake()
     {
-        transformCam    = Camera.main.transform;
-        camStartPos     = transformCam.position;
-    }
-    private void Start()
-    {
-        int backCount = transform.childCount;
-        mats = new Material[backCount];
-        backSpeed = new float[backCount];
-        backgrounds = new GameObject[backCount];
+        camStartPos = camTransform.position;
 
-        for (int i = 0; i < backCount; ++i)
+        int backgroundCount = transform.childCount;
+        GameObject[] backgrounds = new GameObject[backgroundCount];
+
+        materials = new Material[backgroundCount];
+        layerSpeed = new float[backgroundCount];
+
+        for (int i = 0; i < backgroundCount; ++i)
         {
             backgrounds[i] = transform.GetChild(i).gameObject;
-            mats[i] = backgrounds[i].GetComponent<Renderer>().material;
-        }
-        BackSpeedCalculate(backCount);
-    }
-
-    private void BackSpeedCalculate(int backCount)
-    {
-        for (int i = 0; i < backCount; ++i)
-        {
-            if((backgrounds[i].transform.position.z - transformCam.position.z) > farthestBack)
-            {
-                farthestBack = backgrounds[i].transform.position.z - transformCam.position.z;
-            }
-        }
-
-        for (int i = 0; i < backCount; ++i)
-        {
-            if ((backgrounds[i].transform.position.z - transformCam.position.z) > farthestBack)
-            {
-                backSpeed[i] =  1 - (backgrounds[i].transform.position.z - transformCam.position.z) / farthestBack;
-            }
-        }
-    }
-
-    private void LateUpdate()
-    {
-        dis = transformCam.position.x - camStartPos.x;
-
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            float speed = backSpeed[i] * parallaxSpeed;
-            mats[i].SetTextureOffset("_MainTex", new Vector2(dis, 0) * speed);
+            materials[i] = backgrounds[i].GetComponent<Renderer>().material;
         }
     }
 }
+
