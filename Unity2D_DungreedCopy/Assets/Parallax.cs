061590e8e7c75d516ef38d7abdaea260f514bc35
Seoptank <cnpdi1118@naver.com>
2024-03-25 23:35:51 +0900

    aaaa

--- Unity2D_DungreedCopy/Assets/Parallax.cs
+++ Unity2D_DungreedCopy/Assets/Parallax.cs
@@ -4,34 +4,68 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField]
-    private Transform camTransform;
+    private Transform       cam;            // MainCamera
+    private Vector3         camStartPos;
+    private float           distance;       // camStartPos와 현재위치간의 거리
 
-    private Vector3 camStartPos;
-    private float   dis;
-    
-    private Material[]  materials;
-    private float[]     layerSpeed;
+    private GameObject[]    backgrounds;                                        
+    private Material[]      mat;                                        
+    private float[]         backSpeed;
 
+    private float           farthestBack;
+
+    [Range(0.01f,0.1f)]
     [SerializeField]
-    [Range(0.01f, 1.0f)]
-    private float parallaxSpeed;
+    private float           parallaxSpeed;
 
     private void Awake()
     {
-        camStartPos = camTransform.position;
+        cam             = Camera.main.transform;
+        camStartPos     = cam.position;
 
-        int backgroundCount = transform.childCount;
-        GameObject[] backgrounds = new GameObject[backgroundCount];
+        int backCount = transform.childCount;
 
-        materials = new Material[backgroundCount];
-        layerSpeed = new float[backgroundCount];
+        mat             = new Material[backCount];
+        backgrounds     = new GameObject[backCount];
+        backSpeed       = new float[backCount];
 
-        for (int i = 0; i < backgroundCount; ++i)
+        for (int i = 0; i < backCount; ++i)
         {
-            backgrounds[i] = transform.GetChild(i).gameObject;
-            materials[i] = backgrounds[i].GetComponent<Renderer>().material;
+            backgrounds[i]  = transform.GetChild(i).gameObject;
+            mat[i]          = backgrounds[i].GetComponent<Renderer>().material;
+        }
+
+        BackSpeedCalculate(backCount);
+    }
+
+    private void BackSpeedCalculate(int backCount)
+    {
+        for (int i = 0; i < backCount; i++) // 가장 멀리있는 백그라운드 찾기
+        {
+            if((backgrounds[i].transform.position.z - cam.position.z) > farthestBack)
+            {
+                farthestBack = backgrounds[i].transform.position.z - cam.position.z;
+            }
+        }
+
+        for (int i = 0; i < backCount; i++) // background의 스피드 설정
+        {
+            backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
         }
     }
+
+    private void LateUpdate()
+    {
+        distance = cam.position.x - camStartPos.x;
+        transform.position = new Vector3(cam.position.x, transform.position.y, 0);
+
+        for (int i = 0; i < backgrounds.Length; ++i)
+        {
+            float speed = backSpeed[i] * parallaxSpeed;
+            mat[i].SetTextureOffset("_MainTex", new Vector2(distance, 0) * speed);
+        }
+    }
+
+
 }
 
